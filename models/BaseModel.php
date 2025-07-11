51fb5caae5861b71b6b1e16ef2b6bf20743bcc6c
Bingomar <ga19033@ues.edu.sv>
2025-07-11 10:17:24 -0600

    Configuración de conexión

---
+++ models/BaseModel.php
@@ -0,0 +1,35 @@
+<?php
+require_once '../config/Database.php';
+
+abstract class BaseModel {
+    protected $db;
+    protected $table;
+
+    public function __construct() {
+        $database = new Database();
+        $this->db = $database->connect();
+    }
+
+    public function getAll() {
+        $query = "SELECT * FROM {$this->table}";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function getById($id) {
+        $query = "SELECT * FROM {$this->table} WHERE id = :id LIMIT 1";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function delete($id) {
+        $query = "DELETE FROM {$this->table} WHERE id = :id";
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }
+}
+?>
